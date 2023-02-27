3d257816186c6f3f02d7cb7ed42290119db68e71
Giggino <giggino@MacBook-Air-di-Geeno.local>
2023-02-27 10:59:35 +0100

    start ex2

--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,17 +1,34 @@
+import lab01.tdd.CircularList;
+import lab01.tdd.CircularListImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
-    //TODO: test implementation
+    private CircularList list;
 
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+    @BeforeEach
+    void beforeEach(){
+        this.list = new CircularListImpl();
     }
 
+    @Test
+    void testIsInitiallyEmpty(){
+        assertEquals(0, this.list.size());
+    }
+
+    @Test
+    void testElementCanBeAdded(){
+        final int ELEMENT_VALUE = 10;
+        this.list.add(ELEMENT_VALUE);
+        assertEquals(1, this.list.size());
+    }
+   
 }
