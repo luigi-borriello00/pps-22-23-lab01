df230cb11433d214a181104e2cab920918a61b1f
Giggino <giggino@Air-di-Geeno.station>
2023-03-01 13:43:53 +0100

    refactor

--- pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
@@ -2,14 +2,16 @@ package lab01.example.model;
 
 public class SimpleBankAccountWithAtm extends SimpleBankAccount implements BankAccountWithAtm {
 
+
+    private static final int BANK_FEE = 1;
     @Override
     public void deposit(int userID, double amount) {
-        super.deposit(userID, amount - 1);
+        super.deposit(userID, amount - BANK_FEE);
     }
 
     @Override
     public void withdraw(int userID, double amount) {
-        super.withdraw(userID, amount + 1);
+        super.withdraw(userID, amount + BANK_FEE);
     }
 
     public SimpleBankAccountWithAtm(AccountHolder holder, double balance) {
